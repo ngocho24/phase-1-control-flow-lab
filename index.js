e3f047ce537a9267643009faaa376070dd6570d4
Elijah Ngocho <elijahngocho24@gmail.com>
2023-10-04 09:06:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+ 
+     if (distance <= 400) {
+       return "This one is on me!";
+   } else if  (distance > 2000 && distance <= 2500 ) {
+      return "I will gladly pay 30 dollars.";
+    } else if (distance > 2500) {
+  return "No can do.";
+    }
+
+  }
+
 
 function ternaryCheckCity(){
   // Write your code here!
+   ternaryCheckCity (city);{
+let location;
+    return city ==="NYC" ? (location = "Ok, sounds good."): (location ="No go.");
+  }
+  
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switchOnCharmFromTip (tip);{
+    switch(Tip){
+      case 'generous':
+      return 'Thank you so much.'
+      break;
+      case 'not as generous':
+      return 'Thank you.'
+      break;
+      default:
+      return 'Bye.'
+      break;
+  }
+
+}
 }
